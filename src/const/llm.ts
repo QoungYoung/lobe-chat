a9c2f84776f4e0644b7943a114cef4770435031d
Qoung Young <qoungyoung@gmail.com>
2024-01-16 23:45:00 +0800

    Update llm.ts
    修改token限制

--- src/const/llm.ts
+++ src/const/llm.ts
@@ -26,7 +26,7 @@ export const ModelTokens: Record<LanguageModel, number> = {
   [LanguageModel.GPT3_5_1106]: 16_385,
   [LanguageModel.GPT3_5_16K]: 16_385,
   [LanguageModel.GPT4]: 8196,
-  [LanguageModel.GPT4_PREVIEW]: 128_000,
-  [LanguageModel.GPT4_VISION_PREVIEW]: 128_000,
+  [LanguageModel.GPT4_PREVIEW]: 8196,
+  [LanguageModel.GPT4_VISION_PREVIEW]: 8196,
   [LanguageModel.GPT4_32K]: 32_768,
 };
