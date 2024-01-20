c17e088b695fd6704c5341f949cf435eccddca23
Qoung Young <qoungyoung@gmail.com>
2024-01-20 20:14:02 +0800

    修改es

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,13 @@
-const config = require('@lobehub/lint').eslint;
-
-config.extends.push('plugin:@next/next/recommended');
-
-config.rules['unicorn/no-negated-condition'] = 0;
-config.rules['unicorn/prefer-type-error'] = 0;
-config.rules['unicorn/prefer-logical-operator-over-ternary'] = 0;
-config.rules['unicorn/no-null'] = 0;
-config.rules['unicorn/no-typeof-undefined'] = 0;
-config.rules['unicorn/explicit-length-check'] = 0;
-config.rules['unicorn/prefer-code-point'] = 0;
-config.rules['no-extra-boolean-cast'] = 0;
-config.rules['unicorn/no-useless-undefined'] = 0;
-config.rules['react/no-unknown-property'] = 0;
-config.rules['unicorn/prefer-ternary'] = 0;
-config.rules['unicorn/prefer-spread'] = 0;
-
+// const config = require('@lobehub/lint').eslint;
+const config = {
+  // 您的自定义规则和配置
+  extends: [
+      // 基础配置，例如 'eslint:recommended'
+  ],
+  rules: {
+      // 您的自定义规则
+  },
+  // 其他配置选项
+};
+// config.extends.push('plugin:@next/next/recommended');
 module.exports = config;
