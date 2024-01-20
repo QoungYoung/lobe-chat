b9e81b2b32c7de8a92318d5ab9f8b39ab287626d
Qoung Young <qoungyoung@gmail.com>
2024-01-20 20:35:19 +0800

    取消检测

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,8 @@
-// const config = require('@lobehub/lint').eslint;
-const config = {
-  // 您的自定义规则和配置
-  extends: [
-      // 基础配置，例如 'eslint:recommended'
-  ],
-  rules: {
-      // 您的自定义规则
-  },
-  // 其他配置选项
-};
-// config.extends.push('plugin:@next/next/recommended');
+const config = require('@lobehub/lint').eslint;
+
+config.extends.push('plugin:@next/next/recommended');
+
+config.rules = {};
+
+
 module.exports = config;
